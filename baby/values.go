622bcfc770ca018625074ee72aef9c5ba2383325
pedropalombo <pedro.palombo@hotmail.com>
2024-03-20 17:13:00 -0300

    Arrays, Slices & examples

--- baby/values.go
+++ baby/values.go
@@ -6,4 +6,22 @@ import "fmt"
 
 func main()  {
 	fmt.Println("go"+"Lang")
+
+	//simple strings
+	var welcomer string = "Hey hey people"
+	followUp := "Astro here ;)"
+
+	fmt.Printf("%s, %v", welcomer, followUp)
+
+	//arrays & slices
+	names := [3]string{"Kallyadranoch", "Allihanna", "Lenah"}
+	fmt.Println("Godhood:", names)
+
+	gods := []string{"Tenebra", "Azgher", "Lin Wu", "Arsenal"}
+	gods = append(gods, "Wynna")  //slice = append(slice, variable)
+	fmt.Println("Divine:", gods)
+
+
+
+	
 }
